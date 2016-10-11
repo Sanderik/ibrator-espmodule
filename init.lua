42f8d8d317be3fb7b32bf4db872e53f455dac56b
erikeuserr <0878524@hro.nl>
2016-10-11 14:52:51 +0200

    implemented socket connection on the client

--- init.lua
+++ init.lua
@@ -4,18 +4,33 @@ local default_headers = 'Content-Type: application/json\r\n'
 wifi.setmode(wifi.STATIONAP)
 wifi.ap.config({ssid="iBrator", auth=wifi.OPEN})
 
---TODO : Use cjson to filter the connectionToken
 enduser_setup.manual(true)
 enduser_setup.start(
     function()
         print("Connected as :" .. wifi.sta.getip())
         wifi.setmode(wifi.STATION)
         enduser_setup.stop()
-        http.post("http://192.168.2.106/device", default_headers, '{"chipId" : "'.. node.chipid()..'"}"',
-            function(statuscode, data)
-                print(data)
-                server_factory.start_webserver(data)
-            end)
+
+        ws = websocket.createClient()
+        ws:on("connection", function(ws)
+          ws:send(cjson.encode({name = "henk"}))
+          ws:send("yooo")
+          print("got ws connection")
+        end)
+        ws:on("receive", function(_, msg, opcode)
+          print('got message:', msg, opcode) 
+        end)
+        ws:on("close", function(_, status)
+          print('connection closed', status)
+          ws = nil -- required to lua gc the websocket client
+        end)
+        ws:connect("ws://192.168.2.106/ws/vibrate")
+       
+--        http.post("http://192.168.2.106/device", default_headers, '{"chipId" : "'.. node.chipid()..'"}"',
+--            function(statuscode, data)
+--                json = cjson.decode(data)
+--                server_factory.start_webserver(json["connectionToken"])
+--            end)
     end,
     function(err, str)
         print("Error: " .. err ": " .. str)
--- server_factory.lua
+++ server_factory.lua
@@ -5,8 +5,8 @@ server_factory.start_webserver = function(connection_token)
     server = net.createServer(net.TCP)
     server:listen(80,function(conn) 
         conn:on("receive", function(client,request)
-            local buf = "";
-
+            local buf = ""; 
+            
             buf = buf.."<html><body><h2>Your connection token: </h2>"
             buf = buf..connection_token.."<br/><br/>"
             buf = buf.."<i>Use this token to connect your vibrator to your account</i>"
